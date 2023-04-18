f7993579c757c338f64f29fd4728666bade0f14c
Arne Brasseur <arne@arnebrasseur.net>
2023-04-18 10:35:42 -0400

    Some more boolean expressions, gensym for locals
    Prep work for do/let which will require flattening block forms into a sequence
    of statements

---
+++ repl_sessions/print_parse_tree.mjs
@@ -0,0 +1,23 @@
+import * as astring from 'astring'
+import {Parser} from 'acorn'
+const path = 'node:' + 'process'
+const process = await import(path)
+
+
+function print_parse_tree(... code) {
+    for (let c of code) {
+        console.log(c)
+        console.dir(Parser.parse(c, {allowAwaitOutsideFunction: true, ecmaVersion: 8}).body[0], {depth: null})
+    }
+}
+
+print_parse_tree(
+    // '{const x = foo(); const y = bar(x); do_thing(x,y)}',
+    // '{const [x,y] = foo()}',
+    // '{const {x,y} = foo()}',
+    // '{const {x: x_,y: y_} = foo()}',
+    // '{const {x: x_ = 123,y: y_} = foo()}',
+    // 'x = 123'
+    // '{(123)}'
+    '( function() { let x = {(123);}; return x })'
+)
