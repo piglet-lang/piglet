d12a8282183db4ae25f08eb1de827f7f29ee1c93
Arne Brasseur <arne@arnebrasseur.net>
2023-04-23 20:01:23 -0400

    Remove old test code

--- demo/first/foo.js
+++
@@ -1,6 +0,0 @@
-import piglet from "pigletlang/lang"
-import piglet from "pigletlang/NodeCompiler"
-
-piglet.inter("*compiler*", new NodeCompiler(...))
-
-const bun_mod = await piglet.require(piglet.symbol("..."))
--- demo/second/mod2.pig
+++
@@ -1,4 +0,0 @@
-(module second/mod2
-  )
-
-(def inc (fn* (x) (+ x 1)))
