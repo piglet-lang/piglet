0ce4e6dbb20d181969125874da78ae6109c68e1f
Arne Brasseur <arne@arnebrasseur.net>
2023-10-10 18:02:27 -0400

    Some final fixes related to the rollup build
    Fix package.json syntax, drop old extend syntax.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,28 +2,32 @@ import { nodeResolve } from '@rollup/plugin-node-resolve'
 import replace from '@rollup/plugin-replace'
 import terser from '@rollup/plugin-terser'
 
-const browser = {output: {}, plugins: [nodeResolve()]}
-const node = {output: {}, plugins: [nodeResolve()]}
+const browser = {output: {}, plugins: []}
+const node    = {output: {}, plugins: []}
+const lang    = {output: {}, plugins: [terser()]}
 
 browser.onwarn = node.onwarn = (e, warn) => (e.code === 'EVAL') ? "" : warn(e)
 
 browser.input                    = 'lib/piglet/browser/main.mjs'
 browser.output.file              = 'dist/piglet.browser.mjs'
+browser.plugins.push(nodeResolve(), terser())
 
 node.input                       = 'lib/piglet/node/main.mjs'
 node.output.file                 = 'dist/piglet.node.mjs'
 node.output.inlineDynamicImports = true
+node.plugins.push(
+    nodeResolve(),
+    replace({
+        values: {
+            PIGLET_PACKAGE_PATH: 'path.join(createRequire(import.meta.url).resolve("piglet-lang"), "../../packages/piglet")'
+        },
+        preventAssignment: true
+    }),
+    terser()
+)
 
-const node_replace = {
-    values: {
-        PIGLET_PACKAGE_PATH: 'path.join(createRequire(import.meta.url).resolve("piglet-lang"), "../../../packages/piglet")'
-    },
-    preventAssignment: true
-}
+lang.input = 'lib/piglet/lang.mjs'
+lang.output.file = 'dist/lang.mjs'
+lang.plugins.push(terser())
 
-browser.plugins.push(terser())
-
-node.plugins.push(replace(node_replace))
-node.plugins.push(terser())
-
-export default [browser, node]
+export default [browser, node, lang]
